095f87fbb902927877f3d18cd53eaa607c76bd0d
gagalady <ryoheikara90824@icloud.com>
2020-10-17 22:36:30 +0900

    taskappラベル追加

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,24 +7,81 @@
 //
 
 import UIKit
+import RealmSwift
+import UserNotifications
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-@IBOutlet weak var tableView: UITableView!
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate
+ {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var search: UISearchBar!
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        let predicate = NSPredicate(format: "category BEGINSWITH %@", searchText)
+        taskArray = realm.objects(Task.self).filter(predicate)
+        tableView.reloadData()
+//       if text == "" {
+//           search.updateView(arr: arr)
+//        } else {
+//           filterdArr = arr.filter { (str) -> Bool in
+//                return str.contains(text)
+//            }
+//            mainView.updateView(arr: filterdArr)
+//        }
+    }
+    
+    let realm = try! Realm()
+    
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending:
+        true)
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+
+            inputViewController.task = task
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        search.delegate = self
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskArray.count
     }
 
     // 各セルの内容を返すメソッド
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 再利用可能な cell を得る
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
 
         return cell
     }
@@ -40,8 +97,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     // Delete ボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    
+    if editingStyle == .delete {
+        
+        let task = self.taskArray[indexPath.row]
+
+        // ローカル通知をキャンセルする
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+        // データベースから削除する
+        try! realm.write {
+            self.realm.delete(self.taskArray[indexPath.row])
+            tableView.deleteRows(at: [indexPath], with: .fade)
+     }
+        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+            for request in requests {
+                print("/---------------")
+                print(request)
+                print("---------------/")
+            }
+        }
     }
-
-
+  }
 }
-

2ed6eb14220d7d994e6dbbd55772966e21a90f85
gagalady <ryoheikara90824@icloud.com>
2020-10-18 21:12:22 +0900

    taskappラベルの追加

--- InputViewController.swift
+++ InputViewController.swift
@@ -15,6 +15,7 @@ class InputViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var categoryTextView: UITextView!
     
     let realm = try! Realm()
     var task: Task!
@@ -28,12 +29,14 @@ class InputViewController: UIViewController {
             titleTextField.text = task.title
             contentsTextView.text = task.contents
             datePicker.date = task.date
+        categoryTextView.text = task.category
         }
     override func viewWillDisappear(_ animated: Bool) {
         try! realm.write {
             self.task.title = self.titleTextField.text!
             self.task.contents = self.contentsTextView.text
             self.task.date = self.datePicker.date
+            self.task.category = self.categoryTextView.text
             self.realm.add(self.task, update: .modified)
         }
         
